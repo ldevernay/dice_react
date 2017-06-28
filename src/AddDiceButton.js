6b8ffdf9aa036b2c6d88ca554ea0713471904564
ldevernay <boblehamster@yahoo.fr>
2017-06-28 14:40:13 +0200

    shortid dependency

--- src/AddDiceButton.js
+++ src/AddDiceButton.js
@@ -20,18 +20,37 @@ class AddDiceButton extends Component {
     const {count} = this.state
 
     return (
-      <div className="col-sm-2">
-        {count > 0 && (<div className="card small">
-          <div className="section darker">
-            <button onClick={this.handleAdd}>
-              +
-            </button>
-            <p>D{count}</p>
-            <button onClick={this.handleRemove}>
-              -
-            </button>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-1">
+            {count > 0 && (<div className="card small">
+              <div className="section darker">
+                <button onClick={this.handleAdd}>
+                  +
+                </button>
+                <p>D{count}</p>
+                <button onClick={this.handleRemove}>
+                  -
+                </button>
+              </div>
+            </div>)}
           </div>
-        </div>)}
+
+          <div className="col-sm-1">
+            {count > 0 && (<div className="card small">
+              <div className="section darker">
+                <button onClick={this.handleAdd}>
+                  +
+                </button>
+                <p>D{count}</p>
+                <button onClick={this.handleRemove}>
+                  -
+                </button>
+              </div>
+            </div>)}
+          </div>
+
+        </div>
       </div>
     )
   }
